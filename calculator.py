2f8092ca1da7619073a3712760943a5af97b17c9
IsHacker <ishayu301@gmail.com>
2025-02-28 22:18:16 +1100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,18 @@
+#               Copyright (C) 2025  IsHacker
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
 # Import OS module to execute shell commands
 import os
-# A clever method to install and import tkinter as a module
+# A clever method to install and import tkinter as a module (needed if you run this .py file directly)
 print("Checking if tkinter is installed......")
 try:
    from tkinter import *
